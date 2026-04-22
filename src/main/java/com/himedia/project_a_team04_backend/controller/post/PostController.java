9a799aa61fd56e92dcf5d94a4a05705488fb9977
Team04 <team04@wis.com>
2026-04-22 18:00:24 +0900

    feat: 기본 post 파일 구조 생성, 스프링 시큐리티 주석처리

--- src/main/java/com/himedia/project_a_team04_backend/controller/post/PostController.java
+++ src/main/java/com/himedia/project_a_team04_backend/controller/post/PostController.java
@@ -1,4 +1,19 @@
 package com.himedia.project_a_team04_backend.controller.post;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+
+    @GetMapping("test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("PostController is working!");
+    }
+
 }
+
+
--- src/main/java/com/himedia/project_a_team04_backend/exception/post/PostNotFoundException.java
+++ src/main/java/com/himedia/project_a_team04_backend/exception/post/PostNotFoundException.java
@@ -1,7 +1,7 @@
 package com.himedia.project_a_team04_backend.exception.post;
 
 public class PostNotFoundException extends RuntimeException {
-  public PostNotFoundException(String message) {
-    super(message);
-  }
+    public PostNotFoundException(String message) {
+        super(message);
+    }
 }
